6f59418315090f7aca8fbaf1bd570cc17e5ace3d
boonz101 <boonz141@gmail.com>
2023-05-18 01:37:31 +1200

    commit2

---
+++ Pong/src/PongPanel.java
@@ -0,0 +1,41 @@
+import java.awt.Color;
+import javax.swing.JPanel;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class PongPanel extends JPanel implements ActionListener, KeyListener {
+	
+	public static final Color BACKGROUND_COLOR = Color.BLACK;
+	
+	public PongPanel()
+	{
+		setBackground(BACKGROUND_COLOR);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
