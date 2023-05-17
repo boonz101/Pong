7b811b8fef8d14ccccf6d5735afbe41db9bd16a6
boonz101 <boonz141@gmail.com>
2023-05-18 02:12:37 +1200

    commit3

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,4 +1,7 @@
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.Timer;
 import javax.swing.JPanel;
 import java.awt.event.KeyListener;
 import java.awt.event.ActionEvent;
@@ -8,12 +11,23 @@ import java.awt.event.KeyEvent;
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	
 	public static final Color BACKGROUND_COLOR = Color.BLACK;
+	public static final int TIMER_DELAY = 5;
 	
 	public PongPanel()
 	{
 		setBackground(BACKGROUND_COLOR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.start();
 	}
-
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(20, 20, 100, 100);
+	}
+	
 	@Override
 	public void keyTyped(KeyEvent event) {
 		// TODO Auto-generated method stub
@@ -29,6 +43,12 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyReleased(KeyEvent event) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void update(ActionEvent event)
+	{
 		
 	}
 
